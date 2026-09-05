2fb40e4e515894a1e5c3c4afb4992e7e723f3fd3
Nirmal494 <nirmalakarsha74@gmail.com>
2026-09-05 18:46:15 +0530

    Update the projectile file with the new projectile objects

--- projectile_physics_engine.py
+++ projectile_physics_engine.py
@@ -1,31 +1,24 @@
 import math
 from abc import ABC, abstractmethod
 
-
+#Use Abstract in OOP to define a base class for drag models. 
 class DragModel(ABC):
-    """Something that can compute the acceleration acting on a
-    projectile, given its current velocity components."""
-
+    
     @abstractmethod
     def acceleration(self, vx, vy, gravity):
-        """Return (ax, ay) for the given velocity and gravity."""
         raise NotImplementedError
 
-
+#Use inheritance in OOP to define a drag model with no air resistance
 class NoDrag(DragModel):
-    """Gravity only - the classic textbook case."""
-
     def acceleration(self, vx, vy, gravity):
         return 0.0, -gravity
 
 
 class QuadraticDrag(DragModel):
-    """Gravity plus quadratic (speed-squared) air resistance:
-    F_drag = -k * v * |v|, split into its x and y components."""
-
+    
     def __init__(self, drag_coefficient, mass):
-        self._k = drag_coefficient
-        self._mass = mass
+        self._k = drag_coefficient   #Used Encapsulation in OOP to protect the drag_coefficient from being accessed directly
+        self._mass = mass            #Used Ecapsulation in OOP to protect the mass attribute
 
     def acceleration(self, vx, vy, gravity):
         speed = math.hypot(vx, vy)
@@ -35,20 +28,31 @@ class QuadraticDrag(DragModel):
         return -factor * vx, -gravity - factor * vy
 
 
-class ProjectilePhysics:
-    """Simulates a projectile's flight using semi-implicit Euler
-    integration, optionally with quadratic air resistance."""
+class BaseketballModel(QuadraticDrag):
+    def __init__(self):
+        super().__init__(drag_coefficient=0.013, mass=0.624)  # unit of mass is kg
 
+class GolfBallModel(QuadraticDrag):
+    def __init__(self):
+        super().__init__(drag_coefficient=0.000222, mass=0.0459)  # unit of mass is kg
+
+class CannonBallModel(QuadraticDrag):
+    def __init__(self):
+        super().__init__(drag_coefficient=0.00047, mass=4.0)  # unit of mass is kg
+                
+
+class ProjectilePhysics:
+    
     def __init__(self, launchSpeed, launchAngle, startHeight=0.0,
                  gravity=9.81, dragCoefficient=0.0, mass=1.0, dt=0.001):
-        self.launchSpeed = launchSpeed          # m/s
-        self.launchAngle = launchAngle          # degrees
-        self.startHeight = startHeight          # m above the ground
-        self.gravity = gravity                  # m/s^2
-        self.dragCoefficient = dragCoefficient  # how strongly air slows the object
-        self.mass = mass                        # kg
-        self.dt = dt                            # integration timestep (s)
-
+        self.launchSpeed = launchSpeed          # speed of the launched object in m/s
+        self.launchAngle = launchAngle          #The launching angle of the object
+        self.startHeight = startHeight          # The starting height above the ground in meters
+        self.gravity = gravity                  #Gravitational acceleration in m/s^2
+        self.dragCoefficient = dragCoefficient  #How strongly air resistance is slow down the object
+        self.mass = mass                        #Mass of the projectile in kg
+        self.dt = dt                            #integration timesteps(s)
+        
         angle_rad = math.radians(launchAngle)
         self.horizontalVelocity = launchSpeed * math.cos(angle_rad)  # vx0
         self.verticalVelocity = launchSpeed * math.sin(angle_rad)    # vy0
@@ -59,7 +63,7 @@ class ProjectilePhysics:
             QuadraticDrag(dragCoefficient, mass) if dragCoefficient > 0 else NoDrag()
         )
 
-        self.__trajectory = []  # private: (t, x, y, vx, vy) samples
+        self.__trajectory = []  # Use Encapsulation to hide the trajectory data from internal access.
         self._simulate()
 
     # ------------------------------------------------------------------
@@ -75,9 +79,6 @@ class ProjectilePhysics:
         while y >= 0:
             ax, ay = self._drag_model.acceleration(vx, vy, self.gravity)
 
-            # Semi-implicit (symplectic) Euler: update velocity first,
-            # then use the new velocity to update position. More stable
-            # than plain Euler for this kind of motion.
             vx += ax * self.dt
             vy += ay * self.dt
             x += vx * self.dt
@@ -92,7 +93,7 @@ class ProjectilePhysics:
         self.__trajectory = trajectory
 
     def _sample_at(self, t, index):
-        """Shared lookup helper for position_lookup() and velocity_at()."""
+        
         for i in range(len(self.__trajectory) - 1):
             t0 = self.__trajectory[i][0]
             t1 = self.__trajectory[i + 1][0]
@@ -104,31 +105,25 @@ class ProjectilePhysics:
     # Public queries
     # ------------------------------------------------------------------
     def position_lookup(self, t):
-        """Find where the object was at a given time."""
         return self._sample_at(t, 1), self._sample_at(t, 2)
 
-    def velocity_at(self, t):
-        """Find how fast the object was moving at a given time."""
+    def velocity_at(self, t):    
         return self._sample_at(t, 3), self._sample_at(t, 4)
 
-    def max_height(self):
-        """Maximum height reached by the object."""
+    def max_height(self):   
         return max(point[2] for point in self.__trajectory)
 
-    def range(self):
-        """How far the object travelled from launch to landing."""
+    def range(self):    
         return self.__trajectory[-1][1]
 
-    def flight_time(self):
-        """Total time from launch to landing."""
+    def flight_time(self):    
         return self.__trajectory[-1][0]
 
-    def full_trajectory(self):
-        """Returns the full list of (t, x, y, vx, vy) samples."""
+    def full_trajectory(self):    
         return self.__trajectory
 
     def results(self):
-        """Key results, ready to display or plot."""
+        
         return {
             "initial_speed": self.launchSpeed,
             "launch_angle_deg": self.launchAngle,
